77bef7062e0dfd3d1e1178c37c4493d915c6b075
Mahara Zaman <maharazaman16@gmail.com>
2025-09-27 01:57:36 +0600

    overview solved

--- social_media_manager/social_media_manager/urls.py
+++ social_media_manager/social_media_manager/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.views.generic import RedirectView
+from social_media_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home),
-    path('', views.home, name='home'),
+
+    # root -> /overview/ এ রিডাইরেক্ট
+    path('', RedirectView.as_view(pattern_name='overview', permanent=False)),
+
+    path('home/', views.home, name='home'),
+    path('overview/', views.overview, name='overview'),
 ]
--- social_media_manager/social_media_manager/views.py
+++ social_media_manager/social_media_manager/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "overview.html")
+def overview(request):
+    return render(request, "overview.html")
 
